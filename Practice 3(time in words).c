1fce91f57712e1d4350aa1a88303202754e1d831
ifirsandi <45285295+ifirsandi@users.noreply.github.com>
2019-01-14 22:57:34 +0700

    Create Practice 3(time in words).c

---
+++ Practice 3(time in words).c
@@ -0,0 +1,57 @@
+https://www.hackerrank.com/challenges/the-time-in-words/problem
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char words[][21] = {"o' clock", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
+
+int main()
+{
+    int h,m;
+    scanf("%d", &h);
+    //printf("%s", words[h]);
+    scanf("%d", &m);
+    if (m==0){
+        printf("%s %s", words[h], words[m]);
+    }
+    else{ 
+        if (m<=30){
+            if (m==30){
+                printf("half ");
+            }
+            else if (m==15){
+                printf("%s ", words[m]);
+            }
+            else if (m<21){
+                printf("%s minutes ", words[m]);      
+            }
+            else {
+                printf("%s %s minutes ", words[20], words[h-20]);
+            }
+            printf ("past ");
+        }
+        else {
+            m=60-m;
+            if (m==15){
+                printf("%s ", words[m]);
+            }
+            else if (m<21){
+                printf("%s minutes ", words[m]);      
+            }
+            else {
+                printf("%s %s minutes ", words[20], words[h-20]);
+            }
+            printf ("to ");
+            h++;
+        }
+        printf("%s", words[h]);
+    }
+
+    //printf("%s", words[h]);
+
+    return 0;
+}
+
+
